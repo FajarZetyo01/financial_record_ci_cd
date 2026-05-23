afcf5cffdb9be1aabbeb700a52a86ea3449eabf2
PLN <fajarsetyopambudi@gmail,com>
2026-05-23 22:33:56 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,3 +1,69 @@
+//package main
+//
+//import (
+//	"financial_record/config"
+//	"financial_record/routes"
+//
+//	"log"
+//	"net/http"
+//)
+//
+//func main() {
+//	//http.HandleFunc("/testing", func(writer http.ResponseWriter, request *http.Request) {
+//	//	tmpl, err := template.ParseFiles("views/testing.html")
+//	//	if err != nil {
+//	//		http.Error(writer, "Template error", http.StatusInternalServerError)
+//	//		return
+//	//	}
+//	//	//KIRIM DATA DARI GO KE HTML
+//	//	data := make(map[string]interface{})
+//	//	data["name"] = "Fajar Setyo Pambudi"
+//	//	data["training"] = "Golang"
+//	//	data["duration"] = 4
+//	//
+//	//	if request.Method == http.MethodPost {
+//	//		//AMBIL DATA DARI HTML
+//	//		request.ParseForm()
+//	//		username := request.FormValue("username")
+//	//		age := request.FormValue("age")
+//	//		fmt.Println(username, age)
+//	//	}
+//	//	tmpl.Execute(writer, data)
+//	//
+//	//})
+//	//http.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+//	//	tmpl, err := template.ParseFiles("views/auth/login.html")
+//	//	if err != nil {
+//	//		http.Error(writer, "Template error", http.StatusInternalServerError)
+//	//		return
+//	//	}
+//	//	if request.Method == http.MethodPost {
+//	//		//AMBIL DATA DARI HTML
+//	//		request.ParseForm()
+//	//		username := request.FormValue("username")
+//	//		age := request.FormValue("age")
+//	//		fmt.Println(username, age)
+//	//	}
+//	//	tmpl.Execute(writer, nil)
+//	//
+//	//})
+//
+//	config.InitSession()
+//	mux := http.NewServeMux()
+//
+//	//DAFTARKAN DIRECTORI
+//	mux.Handle("/user_photo/", http.StripPrefix("/user_photo/", http.FileServer(http.Dir("public/user_photo"))))
+//
+//	//PANGGIL DATABASE
+//	db := config.InitDatabase()
+//
+//	//PANGGIL ROUTE
+//	routes.Route(db, mux)
+//
+//	log.Println("Service sedang berjalan di http://localhost:8080")
+//	http.ListenAndServe(":8080", config.SessionManager.LoadAndSave(mux))
+//}
+
 package main
 
 import (
@@ -9,57 +75,44 @@ import (
 )
 
 func main() {
-	//http.HandleFunc("/testing", func(writer http.ResponseWriter, request *http.Request) {
-	//	tmpl, err := template.ParseFiles("views/testing.html")
-	//	if err != nil {
-	//		http.Error(writer, "Template error", http.StatusInternalServerError)
-	//		return
-	//	}
-	//	//KIRIM DATA DARI GO KE HTML
-	//	data := make(map[string]interface{})
-	//	data["name"] = "Fajar Setyo Pambudi"
-	//	data["training"] = "Golang"
-	//	data["duration"] = 4
-	//
-	//	if request.Method == http.MethodPost {
-	//		//AMBIL DATA DARI HTML
-	//		request.ParseForm()
-	//		username := request.FormValue("username")
-	//		age := request.FormValue("age")
-	//		fmt.Println(username, age)
-	//	}
-	//	tmpl.Execute(writer, data)
-	//
-	//})
-	//http.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
-	//	tmpl, err := template.ParseFiles("views/auth/login.html")
-	//	if err != nil {
-	//		http.Error(writer, "Template error", http.StatusInternalServerError)
-	//		return
-	//	}
-	//	if request.Method == http.MethodPost {
-	//		//AMBIL DATA DARI HTML
-	//		request.ParseForm()
-	//		username := request.FormValue("username")
-	//		age := request.FormValue("age")
-	//		fmt.Println(username, age)
-	//	}
-	//	tmpl.Execute(writer, nil)
-	//
-	//})
 
+	// INIT SESSION
 	config.InitSession()
+
+	// CREATE MUX
 	mux := http.NewServeMux()
 
-	//DAFTARKAN DIRECTORI
-	mux.Handle("/user_photo/", http.StripPrefix("/user_photo/", http.FileServer(http.Dir("public/user_photo"))))
+	// HEALTH CHECK UNTUK AWS ALB
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	//PANGGIL DATABASE
+	// STATIC FILE
+	mux.Handle(
+		"/user_photo/",
+		http.StripPrefix(
+			"/user_photo/",
+			http.FileServer(http.Dir("public/user_photo")),
+		),
+	)
+
+	// INIT DATABASE
 	db := config.InitDatabase()
 
-	//PANGGIL ROUTE
+	// REGISTER ROUTES
 	routes.Route(db, mux)
 
+	// LOG SERVER
 	log.Println("Service sedang berjalan di http://localhost:8080")
-	http.ListenAndServe(":8080", config.SessionManager.LoadAndSave(mux))
+
+	// RUN SERVER
+	err := http.ListenAndServe(
+		":8080",
+		config.SessionManager.LoadAndSave(mux),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
